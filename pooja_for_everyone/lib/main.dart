6b7cbae200506d44307ae312f9c763e117b352c8
anikettyagi-123 <148858481+anikettyagi-123@users.noreply.github.com>
2024-04-25 13:32:47 +0530

    another change
    another change

--- pooja_for_everyone/lib/main.dart
+++ pooja_for_everyone/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'Splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,7 +20,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const  MaterialApp(
       debugShowCheckedModeBanner: false,
 
       home: SplashScreen()
